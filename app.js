f0bc7f62dd988682eaacdeeaf7066bf3b3bf3898
Brice Pieterse <pietersebrice@gmail.com>
2021-03-31 20:11:42 -0400

    Add form appear and disappear animations

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const compareBtn = document.getElementById("compare");
 const form = document.querySelector(".form-wrapper");
+const formIntro = document.querySelector(".intro-wrapper");
+
 const imperialBtn = document.querySelector(".imperial-btn");
 const metricBtn = document.querySelector(".metric-btn");
+
 const imperialHeight = document.querySelector(".imperial-height-wrapper");
+const heightFeetInput = document.querySelector(".heightFeetInput");
+const heightInchInput = document.querySelector(".heightInchInput");
+
 const metricHeight = document.querySelector(".metric-height-wrapper");
+const heightCmInput = document.querySelector(".heightCmInput");
+
 const weightInput = document.querySelector(".weight-input");
+const nameInput = document.querySelector(".name-input");
+const dietInput = document.querySelector(".diet-input");
 
 
-//reload form animation
+
+//form-in animation
 function loadForm(){
     const keyframes = [
         {
@@ -26,7 +37,32 @@ function loadForm(){
       form.animate(keyframes, options);
 }
 
+
+
+// form-out animation
+function closeForm(){
+    setTimeout(() => {
+        formIntro.style.display = "none"
+    }, 250)
+
+    const keyframes = [
+        {
+          opacity: 1
+        },
+        {
+            opacity: 0
+        }
+      ];
+      const options = {
+          duration: 250,
+          easing: 'ease-out',
+      }
+      formIntro.animate(keyframes, options);
+}
+
 window.onload = loadForm;
+
+
 
 //change form text based on imperial/metric units
 imperialBtn.addEventListener("click", () => {
@@ -40,4 +76,55 @@ metricBtn.addEventListener("click", () => {
   weightInput.placeholder = "Your weight in kg";
 });
 
-compareBtn.addEventListener("click", () => {});
+
+
+// makes sure all form values are filled out
+function formCheck(){
+    let validForm = true;
+    if (!nameInput.value){
+        validForm = false;
+        nameInput.style.border = "1px solid rgb(204, 95, 95)"
+    }
+
+    if (!weightInput.value){
+        validForm = false;
+        weightInput.style.border = "1px solid rgb(204, 95, 95)"
+    }
+    //checks for valid entries for imperial units
+    if (imperialBtn.checked){
+        if (!heightFeetInput.value){
+            validForm = false;
+            heightFeetInput.style.border = "1px solid rgb(204, 95, 95)"
+        }
+        if (!heightInchInput.value){
+            validForm = false;
+            heightInchInput.style.border = "1px solid rgb(204, 95, 95)"
+        }
+    }
+    //checks for valid entries for metric units
+    if (metricBtn.checked){
+        console.log(true)
+        if (!heightCmInput.value){
+            validForm = false;
+            heightCmInput.style.border = "1px solid rgb(204, 95, 95)"
+        }
+    }
+    return validForm;
+}
+
+
+// reset the form for re-use after successful submit
+function resetForm(){
+    nameInput.style.border = "none"
+    weightInput.style.border = "none"
+    heightFeetInput.style.border = "none"
+    heightInchInput.style.border = "none"
+    heightCmInput.style.border = "none"
+}
+
+
+compareBtn.addEventListener("click", () => {
+    if (formCheck()){
+        closeForm();
+    }
+});
